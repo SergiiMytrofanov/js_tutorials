7d39d71bce6e69d7b40574a32bdacf39a96cadce
SergiiMytrofanov <122700399+SergiiMytrofanov@users.noreply.github.com>
2023-10-16 22:09:37 +0300

    update
    update

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,76 @@
-let link = 'http://sergii/mytrofanov/'
-if (link.endsWith('/')){
-  link;
-} else {
-  link += '/'
+
+// let counter = 1;
+// let sum = 0
+// while (counter <=10) {
+//   sum +=counter;
+//   counter +=1;
+// }
+
+// console.log(sum)
+
+// let sum = 0;
+
+// for (let count = 1; count <=25; count +=1 ) {
+// sum += count
+// }
+// console.log(sum)
+
+// const min = 20;
+// const max = 100;
+
+// for (let count = min; count <= max; count +=5 ) {
+// console.log(count)
+// }
+
+// const min = 20;
+// const max = 100;
+
+// for (let count = min; count <= max; count +=1 ) {
+//   if (count % 5 === 0)
+// console.log(count)
+// }
+ 
+
+
+const min = 7;
+const max = 21;
+let count = 0;
+for (let i = min; i <= max; i +=1 ) {
+  if (i % 7 === 0) {
+    count +=1;
+  }
+  
+console.log(count)
 }
 
-console.log(link)
+
+
+// let link = 'http://sergii/mytrofanov/'
+// if (!link.endsWith('/')){
+//   link += "/";
+// } 
+
+// console.log(link)
+
+
+
+// const daysLeft = 5;
+
+// switch (daysLeft) {
+//   case 0:
+//     console.log('Today');
+//     break;
+
+//   case 1:
+//     console.log('Day after');
+//     break;
+
+//   case 2:
+//     console.log('Two days after');
+//     break;
+
+//   default:
+//     console.log('take a rest');
+//     break;
+// }
+
